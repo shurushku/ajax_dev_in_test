ab06401d6a668f5a8cf009c1ecfc6c88e1840b88
shurushku <nastia.kuchina.741456963@gmail.com>
2022-12-23 14:35:30 +0200

    Add base functional to Page class

--- framework/page.py
+++ framework/page.py
@@ -1,10 +1,33 @@
+from selenium.common.exceptions import NoSuchElementException
+from appium.webdriver.webelement import WebElement
+
+
 class Page:
 
     def __init__(self, driver):
         self.driver = driver
 
-    def find_element(self):
-        raise NotImplementedError
+    def check_element_exist(self, locator: tuple) -> bool:
+        try:
+            self.driver.find_element(*locator)
+        except NoSuchElementException:
+            return False
+        else:
+            return True
 
-    def click_element(self):
-        raise NotImplementedError
+    def find_element(self, locator: tuple) -> WebElement:
+        if not self.check_element_exist(locator):
+            raise NoSuchElementException(f'Element {locator} does not exist')
+        element = self.driver.find_element(*locator)
+        return element
+
+    def click_element(self, locator: tuple) -> None:
+        element = self.find_element(locator)
+        element.click()
+
+    def write_to_input(self, locator: tuple, text: str) -> None:
+        element = self.find_element(locator)
+        element.send_keys(text)
+
+    def wait(self, seconds: int):
+        self.driver.implicitly_wait(seconds)
